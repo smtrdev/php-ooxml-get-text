30c5da1f334457739718462b0ea52fcbb96b46ce
Yutaro Shimamura <25090584+shimaji@users.noreply.github.com>
2021-12-04 21:33:10 +0900

    Set up pptx file

--- getTextFromPptx.php
+++ getTextFromPptx.php
@@ -1,1 +1,21 @@
 <?php
+
+$zip = new ZipArchive;
+$response = '';
+
+$filePath = './test.pptx';
+$open = $zip->open($filePath);
+
+if ($open === true) {
+    $slideNumber = 1; //loop through slide files
+    $doc = new DOMDocument;
+    while (($xmlIndex = $zip->locateName('ppt/slides/slide' . $slideNumber . '.xml')) !== false) {
+        $xmlData   = $zip->getFromIndex($xmlIndex);
+        $doc->loadXML($xmlData, LIBXML_NOENT | LIBXML_XINCLUDE | LIBXML_NOERROR | LIBXML_NOWARNING);
+        $response  .= strip_tags($doc->saveXML());
+        $slideNumber++;
+    }
+    $zip->close();
+}
+
+echo $response;
