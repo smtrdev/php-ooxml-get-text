c25d283d8cf6c6c489995ff5a69ec0771e39776d
Yutaro Shimamura <25090584+shimaji@users.noreply.github.com>
2021-12-04 21:26:46 +0900

    Set up xlsx file

--- getTextFromXlsx.php
+++ getTextFromXlsx.php
@@ -1,1 +1,18 @@
 <?php
+
+$xmlFilename = 'xl/sharedStrings.xml';
+$zip = new ZipArchive;
+$response = '';
+
+$filePath = './test.xlsx';
+$open = $zip->open($filePath);
+if ($open === true) {
+    if (($xmlIndex = $zip->locateName($xmlFilename)) !== false) {
+        $doc = new DOMDocument;
+        $xml = $zip->getFromIndex($xmlIndex);
+        $doc->loadXML($xml, LIBXML_NOENT | LIBXML_XINCLUDE | LIBXML_NOERROR | LIBXML_NOWARNING);
+        $response = strip_tags($doc->saveXML());
+        echo $response;
+    }
+    $zip->close();
+}
