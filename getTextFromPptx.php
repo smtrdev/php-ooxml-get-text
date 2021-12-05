0187d39124dc7daada0c7f6c465695fba7d09de0
Yutaro Shimamura <25090584+shimaji@users.noreply.github.com>
2021-12-05 13:07:04 +0900

    Fix pptx file

--- getTextFromPptx.php
+++ getTextFromPptx.php
@@ -1,21 +1,25 @@
 <?php
 
-$zip = new ZipArchive;
-$response = '';
+function getTextFromPptx(string $filePath): string
+{
+    $zip = new ZipArchive;
+    $response = '';
 
-$filePath = './test.pptx';
-$open = $zip->open($filePath);
+    $open = $zip->open($filePath);
 
-if ($open === true) {
-    $slideNumber = 1;
-    $doc = new DOMDocument;
-    while (($xmlIndex = $zip->locateName('ppt/slides/slide' . $slideNumber . '.xml')) !== false) {
-        $xmlData   = $zip->getFromIndex($xmlIndex);
-        $doc->loadXML($xmlData, LIBXML_NOENT | LIBXML_XINCLUDE | LIBXML_NOERROR | LIBXML_NOWARNING);
-        $response  .= strip_tags($doc->saveXML());
-        $slideNumber++;
+    if ($open === true) {
+        $slideNumber = 1;
+        $doc = new DOMDocument;
+        while (($xmlIndex = $zip->locateName('ppt/slides/slide' . $slideNumber . '.xml')) !== false) {
+            $xmlData   = $zip->getFromIndex($xmlIndex);
+            $doc->loadXML($xmlData, LIBXML_NOENT | LIBXML_XINCLUDE | LIBXML_NOERROR | LIBXML_NOWARNING);
+            $response .= strip_tags($doc->saveXML());
+            $slideNumber++;
+        }
+        $zip->close();
     }
-    $zip->close();
+
+    return $response;
 }
 
-echo $response;
+echo getTextFromPptx('./test.pptx');
