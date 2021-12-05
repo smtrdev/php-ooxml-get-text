543eb1ee4069beea3a0bd62c32ff27c6bb3ab87a
Yutaro Shimamura <25090584+shimaji@users.noreply.github.com>
2021-12-05 12:56:52 +0900

    Fix xlsx code

--- getTextFromXlsx.php
+++ getTextFromXlsx.php
@@ -1,18 +1,25 @@
 <?php
 
-$xmlFilename = 'xl/sharedStrings.xml';
-$zip = new ZipArchive;
-$response = '';
 
-$filePath = './test.xlsx';
-$open = $zip->open($filePath);
-if ($open === true) {
-    if (($xmlIndex = $zip->locateName($xmlFilename)) !== false) {
-        $doc = new DOMDocument;
-        $xml = $zip->getFromIndex($xmlIndex);
-        $doc->loadXML($xml, LIBXML_NOENT | LIBXML_XINCLUDE | LIBXML_NOERROR | LIBXML_NOWARNING);
-        $response = strip_tags($doc->saveXML());
-        echo $response;
+
+function getTextFromXlsx(string $filePath): string
+{
+    $zip = new ZipArchive;
+    $response = '';
+
+    $open = $zip->open($filePath);
+    if ($open === true) {
+        $sharedStringsXml = $zip->locateName('xl/sharedStrings.xml');
+        if ($sharedStringsXml) {
+            $domDocument = new DOMDocument;
+            $xml = $zip->getFromIndex($sharedStringsXml);
+            $domDocument->loadXML($xml, LIBXML_NOENT | LIBXML_XINCLUDE | LIBXML_NOERROR | LIBXML_NOWARNING);
+            $response .= strip_tags($domDocument->saveXML());
+        }
+        $zip->close();
     }
-    $zip->close();
+
+    return $response;
 }
+
+echo getTextFromXlsx('./test.xlsx');
