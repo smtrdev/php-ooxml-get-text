6c536aeb06030975980762686f331d369b9e2458
Yutaro Shimamura <25090584+shimaji@users.noreply.github.com>
2021-12-05 12:42:56 +0900

    Fix docx script

--- getTextFromDocx.php
+++ getTextFromDocx.php
@@ -1,11 +1,21 @@
 <?php
-$zip = new ZipArchive;
 
-$filePath = './test.docx';
-$open = $zip->open($filePath);
-if ($open === true) {
-    $documentXml = $zip->getFromName('word/document.xml');
-    $zip->close();
+function getTextFromDocx(string $filePath) {
+    $zip = new ZipArchive;
+    $result = '';
 
-    echo $documentXml;
+    if ($zip->open($filePath) === true) {
+        $documentXml = $zip->getFromName("word/document.xml");
+        if ($documentXml) {
+            $domDocument = new DOMDocument();
+            $domDocument->loadXML($documentXml);
+            $paragraphs = $domDocument->getElementsByTagName("p");
+            /** @var DOMNode $paragraph */
+            foreach ($paragraphs as $paragraph) {
+                $result .= $paragraph->textContent;
+            }
+        }
+    }
 }
+
+getTextFromDocx('./test.docx');
